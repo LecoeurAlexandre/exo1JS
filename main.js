87df3dc85198509b2fce9a2c09296d23beb8865f
Alexandre Lecoeur <alexandre.lecoeur@gmail.com>
2023-01-10 11:06:12 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+let firstname = prompt("Veuillez renseigner votre prénom");
+let lastname = prompt("Veuillez renseigner votre nom");
+
+let msg = `Nous vous apprenons que vous vous appelez ${firstname} ${lastname}`;
+console.log(msg);
